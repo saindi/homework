555430c89396da8f871a9c6c1b6a8efb3ad2217e
saindi <saindi777@gmail.com>
2023-03-18 13:26:03 +0200

    make homework 2

---
+++ src/js/2.js
@@ -0,0 +1,16 @@
+// Перше завдання
+let firstValue = prompt('First value');
+let secondValue = prompt('Second value');
+let thirdValue = prompt('Third value');
+
+let result = firstValue + secondValue + thirdValue;
+
+console.log('1) result: ', result);
+
+
+// Друге завдання
+let number = 12345;
+
+let reversedNumber = number.toString().split('').reverse().join(' ');
+
+console.log('2) result: ', reversedNumber);
